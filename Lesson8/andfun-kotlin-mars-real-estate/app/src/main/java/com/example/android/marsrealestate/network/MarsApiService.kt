f6bf2fe11a7dc4f23ff2f69659463b9f409791f2
Nguyễn Mạnh Hùng <50830861+TamamoNoMae13@users.noreply.github.com>
2021-04-25 16:55:49 +0700

    Exercise 8-9: Coroutines and Deferred

--- Lesson8/andfun-kotlin-mars-real-estate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Lesson8/andfun-kotlin-mars-real-estate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,9 +17,10 @@
 
 package com.example.android.marsrealestate.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -32,17 +33,18 @@ private val moshi = Moshi.Builder()
 
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .baseUrl(BASE_URL)
         .build()
 
 interface MarsApiService {
     @GET("realestate")
     fun getProperties():
-            Call<List<MarsProperty>>
+            Deferred<List<MarsProperty>>
 }
 
 object MarsApi {
-    val retrofitService : MarsApiService by lazy {
+    val retrofitService: MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java)
     }
 }
