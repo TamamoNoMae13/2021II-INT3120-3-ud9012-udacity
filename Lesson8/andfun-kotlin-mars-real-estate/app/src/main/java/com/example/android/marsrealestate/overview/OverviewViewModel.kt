cf2b24fe22c43a07514e17b3306350e922f54db3
Nguyễn Mạnh Hùng <50830861+TamamoNoMae13@users.noreply.github.com>
2021-04-25 17:33:26 +0700

    Exercise 8-13: Error Handling with RecyclerView

--- Lesson8/andfun-kotlin-mars-real-estate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Lesson8/andfun-kotlin-mars-real-estate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -27,14 +27,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 /** The [ViewModel] that is attached to the [OverviewFragment]. */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
-
-    // The external immutable LiveData for the request status String
-    val status: LiveData<String>
+    private val _status = MutableLiveData<MarsApiStatus>()
+    val status: LiveData<MarsApiStatus>
         get() = _status
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
@@ -43,7 +42,7 @@ class OverviewViewModel : ViewModel() {
         get() = _properties
 
     private var viewModelJob = Job()
-    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
     /** Call getMarsRealEstateProperties() on init so we can display status immediately. */
     init {
@@ -55,13 +54,14 @@ class OverviewViewModel : ViewModel() {
         coroutineScope.launch {
             var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
-                var listResult = getPropertiesDeferred.await()
-                _status.value = "Success: ${listResult.size} Mars properties retrieved"
-                if (listResult.size > 0) {
-                    _properties.value = listResult
-                }
+                _status.value = MarsApiStatus.LOADING
+
+                val listResult = getPropertiesDeferred.await()
+                _status.value = MarsApiStatus.DONE
+                _properties.value = listResult
             } catch (e: Exception) {
-                _status.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
             }
         }
     }
