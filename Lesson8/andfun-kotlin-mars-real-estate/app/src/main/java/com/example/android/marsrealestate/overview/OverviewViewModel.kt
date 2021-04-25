d859f37eb0d34c095d2756e715cc7c462c2dfba3
Nguyễn Mạnh Hùng <50830861+TamamoNoMae13@users.noreply.github.com>
2021-04-25 11:34:42 +0700

    Exercise 8-6: Connecting to the Internet

--- Lesson8/andfun-kotlin-mars-real-estate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Lesson8/andfun-kotlin-mars-real-estate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,10 +20,12 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-/**
- * The [ViewModel] that is attached to the [OverviewFragment].
- */
+/** The [ViewModel] that is attached to the [OverviewFragment]. */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
@@ -33,17 +35,21 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
-    /**
-     * Call getMarsRealEstateProperties() on init so we can display status immediately.
-     */
+    /** Call getMarsRealEstateProperties() on init so we can display status immediately. */
     init {
         getMarsRealEstateProperties()
     }
 
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
+    /** Sets the value of the status LiveData to the Mars API status. */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        MarsApi.retrofitService.getProperties().enqueue( object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+        })
     }
 }
